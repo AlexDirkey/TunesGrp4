ca5fee544183837a774814d146ae7ba3fc426bc4
Kimphung3105 <kimngu01@easv365.dk>
2024-12-06 19:35:10 +0100

    Ændring i Album og Artist. Tilføjelse af config.settings

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,5 +1,50 @@
 public class Album
 //must contain Album info. Needs getters and setters.
 {
+private int id;
+private String name;
+private String author;
+private int year;
+private String genre;
+private String playlist;
+
+    public Album(int id, String name, String author, int year, String genre, String playlist) {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public String getPlaylist() {
+        return playlist;
+    }
+    public void setPlaylist(String playlist) {
+        this.playlist = playlist;
+    }
 
 }
--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -1,5 +1,32 @@
 public class Artist
 //Must contain artist info. Needs getters and setters.
 {
+private int id;
+private String name;
+private Album album;
+
+    public Artist(int id, String name, Album album) {
+        this.id = id;
+        this.name = name;
+        this.album = album;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Album getAlbum() {
+        return album;
+    }
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
 
 }

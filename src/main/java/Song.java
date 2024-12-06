75f229b8bc64d4b95ae96f357993e7c0dca3cdcd
AlexDirkey <91528396+AlexDirkey@users.noreply.github.com>
2024-12-06 14:43:10 +0100

    Tilføjet flere metoder til song-classen, er igang med getters og setter (koden skal ses igennem og kommenteres)

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -8,6 +8,22 @@ public class Song // public, så det kan tale sammen med main-metoden
     private Album album;
     private Artist artist;
 
+    public Song() {}
+
+    public Song(int id, String title, String genre, String duration, Album album, Artist artist)
+
+    {
+    this.id = id;
+    this.title = title;
+    this.genre = genre;
+    this.duration = duration;
 
 
 }
+
+//Getters and setters
+public int getId() {return id;}
+public void setId(int id) {this.id = id;}
+public String getTitle() { return title; }
+public void setTitle(String Title) { this.title = title }
+public String setGengre(String Genre) { this.genre = genre }

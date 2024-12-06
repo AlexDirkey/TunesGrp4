926131f823a55f60c5473b5655f022ad99febfd4
AlexDirkey <91528396+AlexDirkey@users.noreply.github.com>
2024-12-06 15:01:41 +0100

    Song-metoden er ved at være i mål, men skal kigges igennem

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -25,5 +25,20 @@ public class Song // public, så det kan tale sammen med main-metoden
 public int getId() {return id;}
 public void setId(int id) {this.id = id;}
 public String getTitle() { return title; }
-public void setTitle(String Title) { this.title = title }
-public String setGengre(String Genre) { this.genre = genre }
+public void setTitle(String Title) { this.title = title; }
+public String getGenre(String Genre) { return genre; }
+    public void setGenre(String Genre) { this.genre = genre; }
+public String getDuration() {return duration;}
+public void setDuration(String duration) {this.duration = duration;}
+
+    public Album getAlbum() {
+        return album;
+    }
+    public void setAlbum(Album album) { this.album = album; }
+
+    public Artist getArtist() {
+        return artist;
+    }
+    public void setArtist(Artist artist) { this.artist = artist;}
+}
+

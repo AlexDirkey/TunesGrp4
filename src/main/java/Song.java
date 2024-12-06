cf1e78cfb467e424d58c9f59b8674a9b84373e5d
AlexDirkey <91528396+AlexDirkey@users.noreply.github.com>
2024-12-06 14:09:01 +0100

    Tilføjet metoder til Song-classen

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -1,5 +1,13 @@
-public class Song
+public class Song // public, så det kan tale sammen med main-metoden
 //Must contain song info. Needs getters and setters.
 {
+    private int id;
+    private String title;
+    private String genre;
+    private String duration;
+    private Album album;
+    private Artist artist;
+
+
 
 }

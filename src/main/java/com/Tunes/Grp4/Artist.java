e3825af79efd1b2e18a8ebef34aae85154e2c7f1
AlexDirkey <91528396+AlexDirkey@users.noreply.github.com>
2024-12-11 11:11:49 +0100

    Kommentarer på Artist.java-classen/ mindre rettelser

--- src/main/java/com/Tunes/Grp4/Artist.java
+++ src/main/java/com/Tunes/Grp4/Artist.java
@@ -1,15 +1,18 @@
+package com.Tunes.Grp4;
 public class Artist
 //Must contain artist info. Needs getters and setters.
 {
-private int id;
-private String name;
-private Album album;
+private int id; // Unique id for the artist
+private String name; // Name of the artist, ex, Bruce Willis
+private Album album; //Name of the album - returns null, if the song isnt on any album
 
-    public Artist(int id, String name, Album album) {
+    public Artist(int id, String name, Album album) {// Constructor for the artist class
         this.id = id;
         this.name = name;
         this.album = album;
     }
+
+    //Getters and setters - gets info from the database and connects it to the GUI
     public int getId() {
         return id;
     }

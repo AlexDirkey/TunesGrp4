e2c9ee599262b9baf1185cc3137c67d49d0fcf67
AlexDirkey <91528396+AlexDirkey@users.noreply.github.com>
2024-12-11 11:01:21 +0100

    Kommentarer på Song-classen/ mindre rettelser

--- src/main/java/Song.java
+++ src/main/java/Song.java
@@ -1,5 +1,7 @@
 public class Song // public, så det kan tale sammen med main-metoden
 //Must contain song info. Needs getters and setters.
+
+    //Definer sang-classen
 {
     private int id;
     private String title;
@@ -8,37 +10,56 @@ public class Song // public, så det kan tale sammen med main-metoden
     private Album album;
     private Artist artist;
 
-    public Song() {}
+    public Song() {} // Constructer - allows creation of a song.
 
-    public Song(int id, String title, String genre, String duration, Album album, Artist artist)
+    public Song(int id, String title, String genre, String duration, Album album, Artist artist) // Song-parameter
 
     {
-    this.id = id;
-    this.title = title;
-    this.genre = genre;
-    this.duration = duration;
+    this.id = id; //Sets a song's id
+    this.title = title; //Sets the songs title
+    this.genre = genre; //Sets the songs genre
+    this.duration = duration; //Sets the duration of the song
 
 
 }
 
-//Getters and setters
-public int getId() {return id;}
-public void setId(int id) {this.id = id;}
-public String getTitle() { return title; }
-public void setTitle(String Title) { this.title = title; }
-public String getGenre(String Genre) { return genre; }
-    public void setGenre(String Genre) { this.genre = genre; }
-public String getDuration() {return duration;}
-public void setDuration(String duration) {this.duration = duration;}
+//Getters and setters - Gets info, id, artist, etc, from our database. They are 'public', as to "talk" with our other classes
+public int getId()
+{return id;}
 
-    public Album getAlbum() {
+public void setId(int id)
+{this.id = id;}
+
+public String getTitle()
+{ return title; }
+
+public void setTitle(String Title)
+{ this.title = title; }
+
+public String getGenre(String Genre)
+{ return genre; }
+
+public void setGenre(String Genre)
+{ this.genre = genre; }
+
+public String getDuration()
+{return duration;}
+
+public void setDuration(String duration)
+{this.duration = duration;}
+
+public Album getAlbum() {
         return album;
     }
-    public void setAlbum(Album album) { this.album = album; }
 
-    public Artist getArtist() {
+public void setAlbum(Album album)
+{ this.album = album; }
+
+public Artist getArtist() {
         return artist;
     }
-    public void setArtist(Artist artist) { this.artist = artist;}
+
+public void setArtist(Artist artist)
+{ this.artist = artist;}
 }
 
